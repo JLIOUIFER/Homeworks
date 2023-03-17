6e16ead6cf9d791dbe420c66b207cf3ac5897ff2
Davit <128072430+JLIOUIFER@users.noreply.github.com>
2023-03-17 16:25:18 +0400

    Add files via upload

---
+++ Day II/Lesson II.py
@@ -0,0 +1,51 @@
+### ჩემთვის შევინახე ესენი
+# name = "Davit" 
+
+# #name არის ცვლადი 
+# # = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# # "Davit" არის ცვლადისთვის მნოშვნელობა
+
+# surname = "Jachvadze"
+
+# print(name)
+
+# print ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+# name = "Davit" # ეს არის str (String) ტიპის ცვლადი
+# age = 24 # ეს არის int (integer) მთელი რიცხვი
+# height = 170.6 # ეს არის (float) ტიპის ცვლადი (ათწილადი)
+# boolean (bool) ტიპის ცვლადი
+
+# knows_programming = True # True ან False
+
+# is_ugly = False # snakecase (უბრალოდ წერის სტილი სტანდარტულად)
+
+# isUgly = False # ჯავასკრიპტული  camelcase
+
+
+# print(name + " " + surname)
+
+# string არის ბრჭყალებში მოქცეული სიმბოლოები
+# print(name + age)
+
+# print(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(height))
+# print(type(knows_programming))
+
+# print(name + " " + str(age))
+
+name = "მე ვარ დავითი ჩემი გვარია"
+surname = "ჯაჭვაძე"
+name2 = "ვარ"
+age = 24
+name3 = "წლის"
+height = 170.6
+name4 = "სმ ოღონდ ზუსტად არ ვიცი"
+
+known_programming = True
+
+is_ugly = False
+
+print(name + " " + surname + " " + name2 + " " + str(age) + " " + name3 + " " + str(height) + " " + name4 + " " + str(known_programming) + " " + str(is_ugly))
